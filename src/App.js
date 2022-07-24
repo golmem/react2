51a28b7dafae12eda647ff459e8cfc418636a610
golmem <toloumpro@gmail.com>
2022-07-24 10:40:39 +0000

    explication1 d'un composant react

--- src/App.js
+++ src/App.js
@@ -1,2 +1,11 @@
-function App() {}
+function App() {
+	//un composant react peut etre decomposer en trois (03) parties
+	/*
+	  //le state (etat, données)
+
+	  //les comportements
+
+	  //affichage (render en anglais et rendu en français)
+	*/
+}
 export default App;
