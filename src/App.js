e58af6bbb5d5e6392e87ec581f8a02f006b0a28a
golmem <toloumpro@gmail.com>
2022-07-24 11:05:52 +0000

    Explication de la communication des boites state,comportement et rendu de REACT

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 function App() {
 	//un composant react peut etre decomposer en trois (03) parties (on peut les appelés des boites)
 	/*
-	  //le state (etat, données)
+	  ------------------------------------------------
+		EXPLICATION DE LA COMMUNICATION DES 03 BOITES
+	  -----------------------------------------------
 
-	  //les comportements
+	  le composant contient d'abord des données ou un état se trouvant au niveau du state(boîte1)
+	  ces données ou etats vont etre affichés au niveau du render (boite3)
+	  ensuite nous pourrons attribuer de manière arbitraire des comportements(boite2) aux elements affichés
+		à travers des evenements
+	  cela va jouer sur les données changeant ainsi le state(boite1)
+	  et reactualisant l'affichage(boite3)
+	  (rerender (terme technique des dev REACT pour parler de reactualisation)) 
 
-	  //affichage (render en anglais et rendu en français)
-
-	  //noté que cet affichage sera possible grace au mot clé return dans lequel on inserera du JSX
-	  //le JSX est un langage inventé par facebook permettant d'ecrire du Javascript dans du Html
-	  //return JSX 
+	  d'où le dynamisme ou reactivité du composant en question
 	*/
 	return <h1>Bonjour Ivan</h1>;
 }
