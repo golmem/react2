6900fc85be2da9be6a294bd1650d874cfdd2c447
golmem <toloumpro@gmail.com>
2022-07-24 13:03:52 +0000

    Liste defini dynamiquement et afficher grace a map

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import { useState } from "react";
 
 function App() {
 	//state(boite1)
-
+	//initialisation de notre state en un tableau d'objet
+	const [fruits, setFruits] = useState([
+		{ id: 1, name: "Abricot" },
+		{ id: 2, name: "Banane" },
+		{ id: 3, name: "Cerise" },
+	]);
 	//comportements
 
 	//render (boite3)
 	return (
 		<div>
-			<h1>Liste de fruitd</h1>
+			<h1>Liste de fruit</h1>
 			<ul>
-				<li>Abricot</li>
-				<li>Banane</li>
-				<li>Cerise</li>
+				{/*
+				 *map va nous permettre de boucler sur notre state fruits
+				 * et de retourner une par une les valeurs a linterieur
+				 * comme une boucle foreach
+				 */}
+
+				{fruits.map((fruit) => {
+					return <li key={fruit.id}>{fruit.name}</li>;
+				})}
 			</ul>
 		</div>
 	);
