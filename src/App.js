1851f501a54650d885b2748b66cc89062193ec3d
golmem <toloumpro@gmail.com>
2022-07-24 10:46:58 +0000

    ajout d'explication et affichage d'un message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 function App() {
-	//un composant react peut etre decomposer en trois (03) parties
+	//un composant react peut etre decomposer en trois (03) parties (on peut les appelés des boites)
 	/*
 	  //le state (etat, données)
 
 	  //les comportements
 
 	  //affichage (render en anglais et rendu en français)
+
+	  //noté que cet affichage sera possible grace au mot clé return dans lequel on inserera du JSX
+	  //le JSX est un langage inventé par facebook permettant d'ecrire du Javascript dans du Html
+	  //return JSX 
 	*/
+	return <h1>Bonjour Ivan</h1>;
 }
 export default App;
