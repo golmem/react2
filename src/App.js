5bc917f2a71151fd4175c9da7a5a8924af5f1c56
golmem <toloumpro@gmail.com>
2022-07-24 11:08:25 +0000

    Comprehension de la notion des boites et de l'interaction entre nos trois boites

--- src/App.js
+++ src/App.js
@@ -1,20 +1,4 @@
 function App() {
-	//un composant react peut etre decomposer en trois (03) parties (on peut les appelés des boites)
-	/*
-	  ------------------------------------------------
-		EXPLICATION DE LA COMMUNICATION DES 03 BOITES
-	  -----------------------------------------------
-
-	  le composant contient d'abord des données ou un état se trouvant au niveau du state(boîte1)
-	  ces données ou etats vont etre affichés au niveau du render (boite3)
-	  ensuite nous pourrons attribuer de manière arbitraire des comportements(boite2) aux elements affichés
-		à travers des evenements
-	  cela va jouer sur les données changeant ainsi le state(boite1)
-	  et reactualisant l'affichage(boite3)
-	  (rerender (terme technique des dev REACT pour parler de reactualisation)) 
-
-	  d'où le dynamisme ou reactivité du composant en question
-	*/
-	return <h1>Bonjour Ivan</h1>;
+	return <h1>1</h1>;
 }
 export default App;
