c78c6939fb045f022a4c4099e6e84c159d625143
golmem <toloumpro@gmail.com>
2022-07-24 12:00:16 +0000

    affectation de notre etat à une variable

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { useState } from "react";
 function App() {
 	//state(boite1)
 	/*
-		//les fonctions speciales de la librairie react sont appélées des Hooks
-		//useState est une fonction Speciale de la librairie react permettant de definir un state
-		//notre fonction useState prendra 1 comme valeur d'initialisation
+		//on crée notre const tableau qui recuperera la valeur du state defini
+		//on utilisera le destructuring pour affecter et modifier notre variable tableau
+		//on aura donc un tableau avec 
+			-tableau pour le nom la variable 
+			-et setTableau comme setter ou mutateur de notre tableau
+			c'est avec le setter que la modification des données de notre variable aura lieu
 	*/
-	useState(1);
+	const [tableau, setTableau] = useState(1);
 
 	//render (boite3)
 	return <h1>1</h1>;
