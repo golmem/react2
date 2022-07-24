512e805d1595b239502b8ad75fbdf885299da9bc
golmem <toloumpro@gmail.com>
2022-07-24 11:23:16 +0000

    Importation de notre premier Hook et defintion de notre premier etat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+//importation de notre premier Hook
+import { useState } from "react";
+
 function App() {
+	//state(boite1)
+	/*
+		//les fonctions speciales de la librairie react sont appélées des Hooks
+		//useState est une fonction Speciale de la librairie react permettant de definir un state
+		//notre fonction useState prendra 1 comme valeur d'initialisation
+	*/
+	useState(1);
+
+	//render (boite3)
 	return <h1>1</h1>;
 }
 export default App;
