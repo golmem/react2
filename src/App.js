a61790c3660bda416156432fda58e4d7ba4101bb
golmem <toloumpro@gmail.com>
2022-07-24 12:46:09 +0000

    gestion de liste

--- src/App.js
+++ src/App.js
@@ -3,23 +3,10 @@ import { useState } from "react";
 
 function App() {
 	//state(boite1)
-	const [compteur, setCompteur] = useState(1);
 
 	//comportements
-	const handleClick = () => {
-		//modifcation de la valeur de compteur
-		setCompteur(compteur + 1);
-	};
+
 	//render (boite3)
-	return (
-		<div>
-			<h1>{compteur}</h1>
-			{/*
-			 *ajout de l'evenement onClick qui signifie au click du bouton appel moi la fonction handleClick
-			 *le handleClick est defini au niveau de la boite2 etant un comportement
-			 */}
-			<button onClick={handleClick}>increment</button>
-		</div>
-	);
+	return <div></div>;
 }
 export default App;
