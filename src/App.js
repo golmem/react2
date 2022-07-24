6ebc94a7ddea7d08a2c643f3a27206836cd2ec92
golmem <toloumpro@gmail.com>
2022-07-24 13:09:29 +0000

    Ajout d'un bouton pour supprimer un fruit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 function App() {
 	//state(boite1)
-	//initialisation de notre state en un tableau d'objet
 	const [fruits, setFruits] = useState([
 		{ id: 1, name: "Abricot" },
 		{ id: 2, name: "Banane" },
@@ -16,14 +15,15 @@ function App() {
 		<div>
 			<h1>Liste de fruit</h1>
 			<ul>
-				{/*
-				 *map va nous permettre de boucler sur notre state fruits
-				 * et de retourner une par une les valeurs a linterieur
-				 * comme une boucle foreach
-				 */}
-
 				{fruits.map((fruit) => {
-					return <li key={fruit.id}>{fruit.name}</li>;
+					return (
+						<div>
+							<li key={fruit.id}>
+								{fruit.name}
+								<button>X</button>
+							</li>
+						</div>
+					);
 				})}
 			</ul>
 		</div>
